107e39f9fb7334cd077e334cd6cd66f7b6fbc331
Rom-99 <rom.avagyan99y@mail.ru>
2023-02-07 18:48:32 +0400

    burger-navbar

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+
 var coll = document.getElementsByClassName("rider__collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("rider__active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
